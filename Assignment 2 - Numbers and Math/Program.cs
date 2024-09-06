7be259a33e8c3ad22d5373cc7a96a5224e389085
Emmett Cornies <339761330@GoTVDSB.ca>
2024-09-06 09:39:27 -0400

    Add project files.

---
+++ Assignment 2 - Numbers and Math/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_2___Numbers_and_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I wll now count my chickens:");
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+            Console.WriteLine("six " + 6);
+            Console.WriteLine("6 + 6");
+            Console.WriteLine("6" + 6);
+            Console.WriteLine(6 + 6);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Now i will count the eggs:");
+            Console.WriteLine(11 / 2.0);
+            Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
+            Console.WriteLine(3 + 2 < 5 - 7);
+            Console.WriteLine("What is 3 + 2 " + (3 + 2));
+
+        }
+    }
+}
