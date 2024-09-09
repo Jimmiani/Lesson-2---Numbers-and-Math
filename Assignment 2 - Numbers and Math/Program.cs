439a40df5cfd7d849c25fbd7a892e1b6f1f4594d
Emmett Cornies <339761330@GoTVDSB.ca>
2024-09-09 09:34:49 -0400

    Done

--- Assignment 2 - Numbers and Math/Program.cs
+++ Assignment 2 - Numbers and Math/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("I wll now count my chickens:");
-            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Hens " + (323 + 5 * 2));
             Console.WriteLine("Roosters " + (25 + 30 / 6));
             Console.WriteLine("six " + 6);
             Console.WriteLine("6 + 6");
@@ -19,7 +19,15 @@
             Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
             Console.WriteLine(3 + 2 < 5 - 7);
             Console.WriteLine("What is 3 + 2 " + (3 + 2));
-
+            Console.WriteLine("What is 5 - 7 " + (5 - 7));
+            Console.WriteLine("Oh that's why it's " + (3 + 2 < 5 - 7));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Today I worked 8 hours! Let's see how much made! " + (21 * 8).ToString("C"));
+            Console.WriteLine("Let's see how much each egg costs!!!! " + (6.0 / 12).ToString("C"));
+            Console.WriteLine("I just bought a BLIGHT sweater for 79.99!! Let's see how much it costs after tax!! " + (79.99 * 1.13).ToString("C"));
+            Console.WriteLine("My little cousin is 5'6!! Let's see how tall he is in centimeters!!" + Math.Round((2.54 * (5 * 12 + 6)), 2) + "cm");
         }
     }
 }
